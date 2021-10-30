da238092e2b3c0e0bfcc41b8ae0d90eda8e6629a
Matt Billock <matt@billock.org>
2021-10-30 14:13:09 -0500

    Calculator is functional

--- epsagon_sample/handler.rb
+++ epsagon_sample/handler.rb
@@ -9,3 +9,36 @@ def hello(event:, context:)
     }.to_json
   }
 end
+
+def calculator(event:, context:)
+  operator = event['operator']
+  success = false
+  result = 0
+  case operator
+  when 'add'
+    result = event['lhs']+event['rhs']
+    success = true
+  when 'subtract'
+    result = event['lhs']-event['rhs']
+    success = true
+  when 'multiply'
+    result = event['lhs']*event['rhs']
+    success = true
+  end
+  if(success)
+    {
+      statusCode: 200,
+      body: {
+        result: result
+      }.to_json
+    }
+  else
+    {
+      statusCode: 500,
+      body: {
+        message: 'The operation was invalid',
+        input: event
+      }.to_json
+    }
+  end
+end
